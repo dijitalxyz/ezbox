ff58b0fd68351393d906ebc16fb1f75874ecf0ff
zetalabs <zetalabs@gmail.com>
2010-06-24 08:13:15 +0000

    remove openwrt/ directory

--- openwrt/target/linux/ifxmips/danube/target.mk
+++
@@ -1,9 +0,0 @@
-ARCH:=mips
-SUBTARGET:=danube
-BOARDNAME:=Danube
-FEATURES:=squashfs jffs2
-
-define Target/Description
-	Lantiq Danube
-endef
-
--- openwrt/target/linux/omap35xx/gumstix/target.mk
+++
@@ -1,7 +0,0 @@
-BOARDNAME:=Gumstix Overo
-LINUX_VERSION:=2.6.34
-
-define Target/Description
-	Build firmware images for Gumstix Overo Boards
-endef
-
