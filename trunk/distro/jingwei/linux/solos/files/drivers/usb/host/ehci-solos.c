0563e0951f3cf37ddbeff14a1126277c1cc29343
zetalabs@gmail.com <zetalabs@gmail.com@2acfdcce-4352-0410-ad0c-411be7f7a404>
2010-10-11 02:37:08 +0000

    fix solos usb host build issue

--- trunk/distro/jingwei/linux/solos/files/drivers/usb/host/ehci-solos.c
+++ trunk/distro/jingwei/linux/solos/files/drivers/usb/host/ehci-solos.c
@@ -15,34 +15,42 @@
  *
  * This file is licenced under the GPL.
  */
-#define EHCI_IAA_JIFFIES        (HZ/100)        /* arbitrary; ~10 msec */
-#define EHCI_IO_JIFFIES         (HZ/10)         /* io watchdog > irq_thresh */
-#define EHCI_ASYNC_JIFFIES      (HZ/20)         /* async idle timeout */
-#define EHCI_SHRINK_JIFFIES     (HZ/200)        /* async qh unlink delay */
-
-
-#include <linux/autoconf.h>
-#include <linux/reboot.h>
-#include <linux/usb.h>
+#include <linux/clk.h>
 #include <linux/platform_device.h>
-#include <mach/io.h>
-#include <asm/mach-types.h>
-#include <mach/hardware.h>
-#include <mach/hardware/usb.h>
-#include <mach/hardware/devconf.h>
-#include <mach/hardware/solosw_gpio.h>
-#include <mach/hardware/solosw_gpio_if.h>
 
-#define EHCI_ULPI_VIEWPORT  0x30       /* RW to send commands to ULPI */
-#define EHCI_PS_PP          0x00001000 /* RW,RO port power */
-/* following define will change for hosts other than arc_usb */
-#define EHCI_USBCMD         0x00       /* RO, RW, WO Command register */
-#define EHCI_CMD_FLS_M      0x0000800c /* RW/RO frame list size */
+/* interface and function clocks */
+static struct clk *iclk, *fclk;
+static int clocked;
 
-struct usbblock {
-	int irq;
-	u32 base_addr;
-};
+/*-------------------------------------------------------------------------*/
+
+static void solos_start_clock(void)
+{
+	//clk_enable(iclk);
+	//clk_enable(fclk);
+	clocked = 1;
+}
+
+static void solos_stop_clock(void)
+{
+	//clk_disable(fclk);
+	//clk_disable(iclk);
+	clocked = 0;
+}
+
+static void solos_start_ehci(struct platform_device *pdev)
+{
+	dev_dbg(&pdev->dev, "start\n");
+	solos_start_clock();
+}
+
+static void solos_stop_ehci(struct platform_device *pdev)
+{
+	dev_dbg(&pdev->dev, "stop\n");
+	solos_stop_clock();
+}
+
+/*-------------------------------------------------------------------------*/
 
 static int ehci_solos_setup(struct usb_hcd *hcd)
 {
@@ -109,156 +117,124 @@ static struct hc_driver ehci_solos_hc_driver = {
 	.port_handed_over	= ehci_port_handed_over,
 };
 
-extern int usb_disabled(void);
-
-static int ehci_hcd_solos_drv_remove(struct device *dev);
-
 /*-------------------------------------------------------------------------*/
+
 static int __init ehci_hcd_solos_drv_probe(struct platform_device *pdev)
 {
-	struct hc_driver	*driver = &ehci_solos_hc_driver;
 	struct usb_hcd		*hcd;
-	struct device		*dev = &pdev->dev;
-	struct usbblock		*block = dev->platform_data;
-	struct ehci_regs *regs = (struct ehci_regs *)((u32)block->base_addr + 0x140);
-	int irq = block->irq;
-	char buf[8], *bufp = buf;
-	int ret;
-	u32 cmd;
-	pr_debug ("In ehci_hcd_solos_drv_probe");
+	const struct hc_driver	*driver = &ehci_solos_hc_driver;
+	struct resource		*res;
+	int irq;
+	int retval;
 
 	if (usb_disabled())
 		return -ENODEV;
 
+	pr_debug ("Initializing Solos-SoC USB Host Controller");
+
+	irq = platform_get_irq(pdev, 0);
+	if (irq <= 0) {
+		dev_err(&pdev->dev,
+			"Found HC with no IRQ. Check %s setup!\n",
+			dev_name(&pdev->dev));
+		retval = -ENODEV;
+		goto fail_create_hcd;
+	}
+
 	hcd = usb_create_hcd(driver, &pdev->dev, dev_name(&pdev->dev));
 	if (hcd == NULL){
-		dev_dbg (dev, "hcd alloc fail\n");
-		return -ENOMEM;
+		dev_dbg(&pdev->dev, "hcd alloc fail\n");
+		retval = -ENOMEM;
+		goto fail_create_hcd;
 	}
 
-#ifdef CONFIG_ARCH_SOLOSW_GALLUS 
-	{
-		int value=0;
-		writel(0x80000000, &regs->port_status[0]);
-		mdelay(20);
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	if (res == NULL) {
+		dev_err(&pdev->dev,
+			"Found HC with no register addr. Check %s setup!\n",
+			dev_name(&pdev->dev));
+		retval = -ENODEV;
+		goto fail_request_resource;
+	}
+	hcd->rsrc_start = res->start;
+	hcd->rsrc_len = res->end - res->start + 1;
 
-		 /* Initialize Stony Point to fix power sequencing problems */
-		value = readl(SOLOS_LAHB_VIRT);
-		if ( value & SOLOS_DEVCONF_CONFIG_EXT_USB_PHY_EN )
-		{
-			writel(0x20040055, VIEWPORT_ADDR);
-			writel(0x60040055, VIEWPORT_ADDR);
-			writel(0x40040055, VIEWPORT_ADDR);
-			writel(0x20040015, VIEWPORT_ADDR);
-			writel(0x60040015, VIEWPORT_ADDR);
-		}
+	if (!request_mem_region(hcd->rsrc_start, hcd->rsrc_len,
+				driver->description)) {
+		dev_dbg(&pdev->dev, "controller already in use\n");
+		retval = -EBUSY;
+		goto fail_request_resource;
 	}
 
-	/* halt the controller */
-	writel(0, &regs->command);
-
-#endif  /* eo CONFIG_ARCH_SOLOSW_GALLUS */ 
-
-	/* Reset the block */
-	cmd = readl(&regs->command);
-	cmd |= CMD_RESET;
-	writel(cmd, &regs->command);
-	while(readl(&regs->command) & CMD_RESET);
-	/* Put it into host mode */
-	writel(FLAG_CF, &regs->configured_flag); 
-	writel(0x13, block->base_addr + 0x1A8);
-	writel(0x1000000, block->base_addr + 0x19c); 
-
-#ifdef CONFIG_ARCH_SOLOS_GALLUS 
-	writel(0, &regs->port_status[1]); 
-#endif /* eo CONFIG_ARCH_SOLOS_GALLUS */
-	
-#ifdef CONFIG_ARCH_SOLOSW_GALLUS 
-
-	if (readl(&regs->port_status[0]) & EHCI_PS_PP)
-	{
-		writel(0x80000000 | EHCI_PS_PP, &regs->port_status[0]);
-	}
-	else
-	{
-		writel(0x80000000, &regs->port_status[0]);
-	}
-	writel(EHCI_CMD_FLS_M, block->base_addr + 0x140);
-#endif /* eo CONFIG_ARCH_SOLOSW_GALLUS */ 
-
-	// hcd zeroed everything
-	hcd->regs = (void *)(block->base_addr + SOLOS_USB_CAPOFFSET);
-	hcd->driver = driver;
-	hcd->self.bus_name = "Solos EHCI";
-	hcd->product_desc = "Solos USB 2.0";
-	hcd->self.controller = &pdev->dev;
-
-	if ((ret = hcd_buffer_create (hcd)) != 0) {
-		usb_put_hcd(hcd);
-		dev_dbg(&pdev->dev, "hcd_buffer_create fail\n");
-		return -ENOMEM;
+	hcd->regs = ioremap_nocache(hcd->rsrc_start, hcd->rsrc_len);
+	if (hcd->regs == NULL) {
+		dev_dbg(&pdev->dev, "error mapping memory\n");
+		retval = -EFAULT;
+		goto fail_ioremap;
 	}
 
-	dev_info (hcd->self.controller, "%s\n", hcd->product_desc);
-
-	/* And mark it as an ARC device */
-	//hcd_to_ehci(hcd)->is_arc_rh_tt = 1;
-	/* till now HC has been in an indeterminate state ... */
-	if (driver->reset && (ret = driver->reset (hcd)) < 0) {
-		dev_err (hcd->self.controller, "can't reset: %d\n", ret);
-		usb_put_hcd(hcd);
-		return ret;
+#if 0
+	iclk = clk_get(&pdev->dev, "ehci_clk");
+	if (IS_ERR(iclk)) {
+		dev_err(&pdev->dev, "Error getting interface clock");
+		retval = -ENOENT;
+		goto fail_get_iclk;
 	}
-	hcd->state = HC_STATE_HALT;
-
-	sprintf (buf, "%d", irq);
-
-	ret = request_irq(irq, usb_hcd_irq, IRQF_SHARED,
-				hcd->product_desc, hcd);
-	if (ret != 0) {
-		dev_err (hcd->self.controller,
-				"request interrupt %s failed\n", bufp);
-		usb_put_hcd(hcd);
-		return -EINVAL;
+	fclk = clk_get(&pdev->dev, "uhpck");
+	if (IS_ERR(fclk)) {
+		dev_err(&pdev->dev, "Error getting function clock\n");
+		retval = -ENOENT;
+		goto fail_get_fclk;
 	}
-	hcd->irq = irq;
+#endif
 
-	dev_info (hcd->self.controller, "irq %s, %s %p\n", bufp,
-		(driver->flags & HCD_MEMORY) ? "pci mem" : "io base",
-		hcd->regs);
+	solos_start_ehci(pdev);
 
-	dev_set_drvdata (dev, hcd);
+	retval = usb_add_hcd(hcd, irq, IRQF_SHARED);
+	if (retval)
+		goto fail_add_hcd;
 
-	if ((ret = driver->start (hcd)) < 0) {
-		dev_err (hcd->self.controller, "init error %d\n", ret);
-		ehci_hcd_solos_drv_remove(dev);
-	}
+	return retval;
 
-	return ret;
+fail_add_hcd:
+	solos_stop_ehci(pdev);
+#if 0
+	//clk_put(fclk);
+//fail_get_fclk:
+	//clk_put(iclk);
+//fail_get_iclk:
+#endif
+	iounmap(hcd->regs);
+fail_ioremap:
+	release_mem_region(hcd->rsrc_start, hcd->rsrc_len);
+fail_request_resource:
+	usb_put_hcd(hcd);
+fail_create_hcd:
+	dev_err(&pdev->dev, "init %s fail, %d\n",
+		dev_name(&pdev->dev), retval);
+
+	return retval;
 }
 
-static int __exit ehci_hcd_solos_drv_remove(struct device *dev)
+static int __exit ehci_hcd_solos_drv_remove(struct platform_device *pdev)
 {
-	struct usb_hcd *hcd = dev_get_drvdata(dev); 
-	if (!hcd)
+	struct usb_hcd *hcd = platform_get_drvdata(pdev);
+
+	if (!hcd) {
+		printk(KERN_INFO "hcd == null\n");
 		return 0;
-	dev_info (hcd->self.controller, "remove, state %x\n", hcd->state);
+	}
 
-	if (in_interrupt ())
-		BUG ();
+	ehci_shutdown(hcd);
+	usb_remove_hcd(hcd);
+	iounmap(hcd->regs);
+	release_mem_region(hcd->rsrc_start, hcd->rsrc_len);
+	usb_put_hcd(hcd);
 
-	if (HC_IS_RUNNING (hcd->state))
-		hcd->state = HC_STATE_QUIESCING;
-
-	dev_dbg (hcd->self.controller, "roothub graceful disconnect\n");
-	usb_disconnect (&hcd->self.root_hub);
-
-	hcd->driver->stop (hcd);
-	hcd_buffer_destroy (hcd);
-	hcd->state = HC_STATE_HALT;
-	dev_set_drvdata(dev, NULL);
-
-	free_irq (hcd->irq, hcd);
+	solos_stop_ehci(pdev);
+	clk_put(fclk);
+	clk_put(iclk);
+	fclk = iclk = NULL;
 
 	return 0;
 }
