8c399ce710af8abe5e142bc9d5fe6b1644f5d596
zetalabs <zetalabs@gmail.com>
2010-10-27 04:12:47 +0000

    add EZBOX on TL-WR740N v4 board support for distro kuafu

--- distro/kuafu/target/linux/ar71xx/files/arch/mips/ar71xx/mach-ezbox-tl-wr740n_v4.c
+++ distro/kuafu/target/linux/ar71xx/files/arch/mips/ar71xx/mach-ezbox-tl-wr740n_v4.c
@@ -1,5 +1,5 @@
 /*
- *  TP-LINK TL-WR740N v4 board support
+ *  EZBOX on TL-WR740N v4 board support
  *
  *  Copyright (C) 2009-2010 Gabor Juhos <juhosg@openwrt.org>
  *  Copyright (C) 2010      Zeta Labs <zetalabs@gmail.com>
@@ -22,16 +22,16 @@
 #include "dev-gpio-buttons.h"
 #include "dev-leds-gpio.h"
 
-#define TL_WR740N_V4_GPIO_LED_QSS		0
-#define TL_WR740N_V4_GPIO_LED_SYSTEM	1
+#define EZBOX_TL_WR740N_V4_GPIO_LED_QSS		0
+#define EZBOX_TL_WR740N_V4_GPIO_LED_SYSTEM	1
 
-#define TL_WR740N_V4_GPIO_BTN_RESET	11
-#define TL_WR740N_V4_GPIO_BTN_QSS		12
+#define EZBOX_TL_WR740N_V4_GPIO_BTN_RESET	11
+#define EZBOX_TL_WR740N_V4_GPIO_BTN_QSS		12
 
-#define TL_WR740N_V4_BUTTONS_POLL_INTERVAL	20
+#define EZBOX_TL_WR740N_V4_BUTTONS_POLL_INTERVAL	20
 
 #ifdef CONFIG_MTD_PARTITIONS
-static struct mtd_partition tl_wr740n_v4_partitions[] = {
+static struct mtd_partition ezbox_tl_wr740n_v4_partitions[] = {
 	{
 		.name		= "u-boot",
 		.offset		= 0,
@@ -67,56 +67,56 @@ static struct mtd_partition tl_wr740n_v4_partitions[] = {
 };
 #endif /* CONFIG_MTD_PARTITIONS */
 
-static struct flash_platform_data tl_wr740n_v4_flash_data = {
+static struct flash_platform_data ezbox_tl_wr740n_v4_flash_data = {
 #ifdef CONFIG_MTD_PARTITIONS
-        .parts          = tl_wr740n_v4_partitions,
-        .nr_parts       = ARRAY_SIZE(tl_wr740n_v4_partitions),
+        .parts          = ezbox_tl_wr740n_v4_partitions,
+        .nr_parts       = ARRAY_SIZE(ezbox_tl_wr740n_v4_partitions),
 #endif
 };
 
-static struct gpio_led tl_wr740n_v4_leds_gpio[] __initdata = {
+static struct gpio_led ezbox_tl_wr740n_v4_leds_gpio[] __initdata = {
 	{
-		.name		= "tl-wr740n-v4:green:system",
-		.gpio		= TL_WR740N_V4_GPIO_LED_SYSTEM,
+		.name		= "ezbox-tl-wr740n-v4:green:system",
+		.gpio		= EZBOX_TL_WR740N_V4_GPIO_LED_SYSTEM,
 		.active_low	= 1,
 	}, {
-		.name		= "tl-wr740n-v4:green:qss",
-		.gpio		= TL_WR740N_V4_GPIO_LED_QSS,
+		.name		= "ezbox-tl-wr740n-v4:green:qss",
+		.gpio		= EZBOX_TL_WR740N_V4_GPIO_LED_QSS,
 		.active_low	= 1,
 	}
 };
 
-static struct gpio_button tl_wr740n_v4_gpio_buttons[] __initdata = {
+static struct gpio_button ezbox_tl_wr740n_v4_gpio_buttons[] __initdata = {
 	{
 		.desc		= "reset",
 		.type		= EV_KEY,
 		.code		= KEY_RESTART,
 		.threshold	= 3,
-		.gpio		= TL_WR740N_V4_GPIO_BTN_RESET,
+		.gpio		= EZBOX_TL_WR740N_V4_GPIO_BTN_RESET,
 		.active_low	= 1,
 	}, {
 		.desc		= "qss",
 		.type		= EV_KEY,
 		.code		= KEY_WPS_BUTTON,
 		.threshold	= 3,
-		.gpio		= TL_WR740N_V4_GPIO_BTN_QSS,
+		.gpio		= EZBOX_TL_WR740N_V4_GPIO_BTN_QSS,
 		.active_low	= 1,
 	}
 };
 
-static void __init tl_wr740n_v4_setup(void)
+static void __init ezbox_tl_wr740n_v4_setup(void)
 {
 	u8 *mac = (u8 *) KSEG1ADDR(0x1f01fc00);
 	u8 *ee = (u8 *) KSEG1ADDR(0x1fff1000);
 
-	ar71xx_add_device_m25p80(&tl_wr740n_v4_flash_data);
+	ar71xx_add_device_m25p80(&ezbox_tl_wr740n_v4_flash_data);
 
-	ar71xx_add_device_leds_gpio(-1, ARRAY_SIZE(tl_wr740n_v4_leds_gpio),
-					tl_wr740n_v4_leds_gpio);
+	ar71xx_add_device_leds_gpio(-1, ARRAY_SIZE(ezbox_tl_wr740n_v4_leds_gpio),
+					ezbox_tl_wr740n_v4_leds_gpio);
 
-	ar71xx_add_device_gpio_buttons(-1, TL_WR740N_V4_BUTTONS_POLL_INTERVAL,
-					ARRAY_SIZE(tl_wr740n_v4_gpio_buttons),
-					tl_wr740n_v4_gpio_buttons);
+	ar71xx_add_device_gpio_buttons(-1, EZBOX_TL_WR740N_V4_BUTTONS_POLL_INTERVAL,
+					ARRAY_SIZE(ezbox_tl_wr740n_v4_gpio_buttons),
+					ezbox_tl_wr740n_v4_gpio_buttons);
 
 	ar71xx_eth1_data.has_ar7240_switch = 1;
 	ar71xx_init_mac(ar71xx_eth0_data.mac_addr, mac, 0);
@@ -138,5 +138,5 @@ static void __init tl_wr740n_v4_setup(void)
 
 	ap91_pci_init(ee, mac);
 }
-MIPS_MACHINE(AR71XX_MACH_TL_WR740N_V4, "TL-WR740N-v4", "TP-LINK TL-WR740N v4",
-	     tl_wr740n_v4_setup);
+MIPS_MACHINE(AR71XX_MACH_EZBOX_TL_WR740N_V4, "EZBOX-TL-WR740N-v4", "EZBOX on TL-WR740N v4",
+	     ezbox_tl_wr740n_v4_setup);
