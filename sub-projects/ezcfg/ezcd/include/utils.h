f9ad64c94d97eb59ba0f8fc5fa9ff3d706176266
zetalabs <zetalabs@2acfdcce-4352-0410-ad0c-411be7f7a404>
2010-11-05 01:46:39 +0000

    add missing utils.h for ezcfg

---
+++ sub-projects/ezcfg/ezcd/include/utils.h
@@ -0,0 +1,39 @@
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+#define STRUCT_LEN(name)	sizeof(name)/sizeof(name[0])
+
+/* shell command */
+#define CMD_CAT			"/bin/cat"
+#define CMD_EZCD		"/sbin/ezcd"
+#define CMD_HOTPLUG2		"/sbin/hotplug2"
+#define CMD_IFCONFIG		"/sbin/ifconfig"
+#define CMD_IFDOWN		"/sbin/ifdown"
+#define CMD_IFUP		"/sbin/ifup"
+#define CMD_KILLALL		"/usr/bin/killall"
+#define CMD_UDEVTRIGGER		"/sbin/udevtrigger"
+
+/* root HOME path */
+#define ROOT_HOME_PATH		"/root"
+
+typedef struct rc_func_s {
+	char *name;
+	int flag;
+	int (*func)(int flag);
+} rc_func_t;
+
+typedef struct proc_stat_s {
+	int pid;
+	char state;
+} proc_stat_t;
+
+/* rc state */
+enum {
+	RC_BOOT,
+	RC_RESTART,
+	RC_STOP,
+	RC_START,
+};
+
+#endif
+
