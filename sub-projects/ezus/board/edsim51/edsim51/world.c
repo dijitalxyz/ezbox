fef7ec92f97fe9ffb52b57e7943f9bc695ea2750
zetalabs <zetalabs@2acfdcce-4352-0410-ad0c-411be7f7a404>
2010-12-19 22:41:37 +0000

    add space_init and time_init for edsim51

--- sub-projects/ezus/board/edsim51/edsim51/world.c
+++ sub-projects/ezus/board/edsim51/edsim51/world.c
@@ -2,6 +2,23 @@
 #include <8051.h>
 #include <ezus.h>
 
+void w_space_init(void)
+{
+	/* setup stack pointer */
+	SP = 0x30 - 1;
+}
+
+void w_time_init(void)
+{
+	
+}
+
+void w_start_world(void *e)
+{
+	if (e == NULL)
+		return;
+}
+
 /*
  * world layer is defined by user's requirement
  * and implemented by system programmer.
@@ -10,11 +27,15 @@
  */
 void world(void)
 {
-	init();
-}
+	/* setup the space of this world */
+	w_space_init();
 
-void w_start_world(void *e)
-{
-	if (e == NULL)
-		return;
+	/* setup the time of this world */
+	w_time_init();
+
+	/* start up the user's applications */
+	/* init() is an ever-last loop routine */
+	init();
+
+	/* should not reach here in a normal running */
 }
--- sub-projects/ezus/board/edsim51/edsim51/world.h
+++ sub-projects/ezus/board/edsim51/edsim51/world.h
@@ -1,6 +1,8 @@
 #ifndef _WORLD_H_
 #define _WORLD_H_
 
+extern void w_space_init(void);
+extern void w_time_init(void);
 extern void w_start_world(void *e);
 
 #endif
