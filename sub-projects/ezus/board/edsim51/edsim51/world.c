7c2eb83a008dc077b69c26108d19545cd088d9fe
zetalabs@gmail.com <zetalabs@gmail.com@2acfdcce-4352-0410-ad0c-411be7f7a404>
2010-12-22 10:16:19 +0000

    add w_thread_init()

--- sub-projects/ezus/board/edsim51/edsim51/world.c
+++ sub-projects/ezus/board/edsim51/edsim51/world.c
@@ -10,6 +10,11 @@
 uint32_t world_uptime_seconds;	/* seconds since world starts */
 uint32_t world_time_ticks;	/* time ticks since last update of world_uptime_seconds */
 uint8_t  world_time_flags;	/* bit 0: whether do w_schedule_threads() within one tick */
+
+uint8_t world_cur_thread_id;	/* current running thread ID */
+
+uint8_t world_thread_quantum[W_THREAD_NUM];	/* thread running quantum of ticks */
+
 /*-------------------------*/
 
 uint8_t w_space_init(void)
@@ -50,54 +55,37 @@ uint8_t w_time_init(void)
 	return 0;
 }
 
-/* current running thread ID */
-uint8_t w_cur_thread_id = 0;
-
-/* thread running state */
-/* 0 : wait; 1 : ready */
-uint8_t w_thread_state = 0x01;
-
-void (* const w_thread_list[W_THREAD_NUM])(void) = {
+void (* const world_thread_list[W_THREAD_NUM])(void) = {
 	init,
 	thread1
 };
 
-void w_thread_context_switch(void)
+void w_thread_context_switch(uint8_t tid)
 {
-	return ;
+	if (world_cur_thread_id == tid) {
+		return ;
+	}
 }
 
 void w_schedule_threads(void)
 {
 	uint8_t i;
-	uint8_t mask;
 
 	i = W_THREAD_NUM - 1;
-	mask = (0x01 << i);
 	while (i > 0) {
-		if ((mask & w_thread_state) == 1) {
-			w_thread_context_switch();
-			w_thread_list[i]();
+		if (world_thread_quantum[i] > 0) {
+			w_thread_context_switch(i);
+			/* after context switch, SP point to i-th thread */
+			return;
 		}
 		i--;
 	}
 }
 
-#if 0
-void w_world_start()
-{
-	/* global interrupt enable */
-	EA = 1;
-
-	while(1) {
-		w_schedule_threads();
-	}
-}
-#endif
-
-
 void w_time_int_service(void)
 {
+	uint8_t i;
+
 	TF0 = 0;
 	TL0 = TL0 + (TIMER0_COUNT & 0x00ff);
 	TH0 = TH0 + (TIMER0_COUNT >> 8);
@@ -110,13 +98,31 @@ void w_time_int_service(void)
 		world_uptime_seconds++;
 	}
 
-	/* do the schedule */
-	if (world_time_flags & 0x01) {
-		/* we have do w_schedule_threads() within one tick */
-		/* so do nothingh right now. */
+	/* decrease threads quantum */
+	i = W_THREAD_NUM - 1;
+	while (i > 0) {
+		if (world_thread_quantum[i] > 0) {
+			world_thread_quantum[i]--;
+		}
+		i--;
 	}
-	else {
-		w_schedule_threads();
+
+	/* signal scheduler */
+	/* w_schedule_threads(); */
+}
+
+void w_thread_init(void)
+{
+	uint8_t i;
+
+	world_cur_thread_id = 0;
+
+	world_thread_quantum[0] = 10;	/* 10 ticks for init() */
+
+	i = W_THREAD_NUM - 1;
+	while (i > 0) {
+		world_thread_quantum[i] = 10;	/* 10 ticks for a thread running */
+		i--;
 	}
 }
 
@@ -134,12 +140,11 @@ void world(void)
 	/* setup the time of this world */
 	w_time_init();
 
+	/* setup threads in this world */
+	w_thread_init();
+
 	/* start up the user's applications */
-#if 0
-	/* w_start_world() is an ever-last loop routine */
-	w_world_start();
-#endif
-	/* init() is an ever-last loop routine */
+	/* init() is an ever-lasting loop routine */
 	init();
 
 	/* should not reach here in a normal running */
