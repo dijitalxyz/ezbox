69d95f685932fb6df8a26841d02952fe44068f1b
zetalabs <zetalabs@2acfdcce-4352-0410-ad0c-411be7f7a404>
2011-01-05 10:07:37 +0000

    add Conexant CX9461X support in kuafu

---
+++ distro/kuafu/target/linux/cx9461x/files-2.6.32/arch/arm/mach-solos/board.h
@@ -0,0 +1,14 @@
+/*
+ * linux/arch/arm/mach-solos/board.h
+ *
+ * Author: Guido Barzini
+ * Copyright (C) 2005 Conexant inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ */
+
+/* Hook for board-specific initialisation */
+extern void solos_board_init(void);
