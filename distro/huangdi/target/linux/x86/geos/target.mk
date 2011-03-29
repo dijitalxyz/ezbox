d034fbc3357c8dba1b0baad19e0b656c8a2de67e
zetalabs <zetalabs@2acfdcce-4352-0410-ad0c-411be7f7a404>
2011-03-29 07:36:26 +0000

    sync huangdi distro with openwrt 20110329

--- distro/huangdi/target/linux/x86/geos/target.mk
+++ distro/huangdi/target/linux/x86/geos/target.mk
@@ -1,7 +1,13 @@
 BOARDNAME:=Traverse Geos
-FEATURES:=squashfs pci usb gpio
-DEFAULT_PACKAGES += kmod-ata-via-sata kmod-crypto-hw-geode \
-            kmod-gpio-cs5535 kmod-gpio-nsc \
+FEATURES:=squashfs jffs2 ext4 pci usb gpio
+ifeq ($(strip $(call CompareKernelPatchVer,$(KERNEL_PATCHVER),ge,2.6.38)),1)
+  GEOS_GPIO:=gpio-cs5535-new
+else
+  GEOS_GPIO:=gpio-cs5535
+endif
+DEFAULT_PACKAGES += kmod-ata-via-sata \
+            kmod-crypto-hw-geode kmod-crypto-ocf \
+            kmod-$(GEOS_GPIO) kmod-gpio-nsc \
             kmod-wdt-geode kmod-hwmon-core kmod-hwmon-lm90 \
             kmod-8139cp kmod-solos-pci \
             kmod-i2c-core kmod-i2c-gpio \
@@ -9,7 +15,9 @@ DEFAULT_PACKAGES += kmod-ata-via-sata kmod-crypto-hw-geode \
             kmod-usb-core kmod-usb2 kmod-usb-uhci \
             kmod-cfg80211 kmod-mac80211 \
             kmod-mppe kmod-pppoe kmod-pppoa kmod-pppol2tp \
-            kmod-ath kmod-ath5k kmod-ath9k
+            kmod-ath kmod-ath5k kmod-ath9k \
+            soloscli linux-atm br2684ctl bridge ppp ppp-mod-pppoa \
+            libopenssl ocf-crypto-headers zlib hwclock hostapd
 
 CS5535_MASK:=0x0E000048
 
@@ -20,6 +28,6 @@ define Target/Description
     Build firmware images for Traverse Geos board
 endef
 
-define KernelPackage/gpio-cs5535/install
-     sed -i -r -e 's/$$$$$$$$/ mask=$(CS5535_MASK)/' $(1)/etc/modules.d/??-gpio-cs5535
+define KernelPackage/$(GEOS_GPIO)/install
+     sed -i -r -e 's/$$$$$$$$/ mask=$(CS5535_MASK)/' $(1)/etc/modules.d/??-$(GEOS_GPIO)
 endef
