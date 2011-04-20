6bb0e37d8221d51d76438698c5d8f0c2f959130e
zetalabs <zetalabs@2acfdcce-4352-0410-ad0c-411be7f7a404>
2011-04-20 09:11:03 +0000

    add missing file ezcfg-sys.h in ezcfg

---
+++ sub-projects/ezcfg/libezcfg/include/ezcfg-sys.h
@@ -0,0 +1,26 @@
+/* ============================================================================
+ * Project Name : ezbox configuration utilities
+ * File Name    : ezcfg-sys.h
+ *
+ * Description  : interface to configurate ezbox information
+ *
+ * Copyright (C) 2010 by ezbox-project
+ *
+ * History      Rev       Description
+ * 2010-11-17   0.1       Write it from scratch
+ * ============================================================================
+ */
+
+#ifndef _EZCFG_SYS_H_
+#define _EZCFG_SYS_H_
+
+/* ezcfg nvram name prefix */
+#define EZCFG_SYS_NVRAM_PREFIX        "sys_"
+
+/* ezcfg system names */
+#define EZCFG_SYS_DEVICE_NAME             "device_name"
+#define EZCFG_SYS_SERIAL_NUMBER           "serial_number"
+#define EZCFG_SYS_HARDWARE_VERSION        "hardware_version"
+#define EZCFG_SYS_SOFTWARE_VERSION        "software_version"
+
+#endif
