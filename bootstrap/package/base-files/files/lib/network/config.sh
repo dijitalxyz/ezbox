1067ba42c90bd8a29a1fb21552731df825a4d778
zetalabs <zetalabs@2acfdcce-4352-0410-ad0c-411be7f7a404>
2011-06-03 02:08:40 +0000

    sync ezbox bootstrap with openwrt-20110601 480dfda6ecb5c30c05ccb591901f618055e29326

--- bootstrap/package/base-files/files/lib/network/config.sh
+++ bootstrap/package/base-files/files/lib/network/config.sh
@@ -186,11 +186,12 @@ prepare_interface() {
 					config_get_bool stp "$config" stp 0
 					$DEBUG brctl addbr "br-$config"
 					$DEBUG brctl setfd "br-$config" 0
-					$DEBUG ifconfig "br-$config" up
 					$DEBUG ifconfig "$iface" 0.0.0.0
 					$DEBUG do_sysctl "net.ipv6.conf.$iface.disable_ipv6" 1
 					$DEBUG brctl addif "br-$config" "$iface"
 					$DEBUG brctl stp "br-$config" $stp
+					[ -z "$macaddr" ] && macaddr="$(cat /sys/class/net/$iface/address)"
+					$DEBUG ifconfig "br-$config" hw ether $macaddr up
 					# Creating the bridge here will have triggered a hotplug event, which will
 					# result in another setup_interface() call, so we simply stop processing
 					# the current event at this point.
