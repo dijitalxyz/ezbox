9abaf9f01d345e97e9deeecd0ee6fe90840d7de4
zetalabs <zetalabs@2acfdcce-4352-0410-ad0c-411be7f7a404>
2011-08-30 03:55:39 +0000

    update distro jingwei to use pool/

--- distro/jingwei/target/linux/solos/files/arch/arm/mach-solos/include/mach/dma.h
+++
@@ -1,8 +0,0 @@
-/*
- * linux/include/asm-arm/arch-solos/dma.h
- * (C) Copyright 2004 Conexant
- */
-#ifndef __ASM_ARCH_DMA_H
-#define __ASM_ARCH_DMA_H
-#define MAX_DMA_ADDRESS          0xffffffff
-#endif				/* __ASM_ARCH_DMA_H */
