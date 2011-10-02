4fe6da1bbc1704d491a89daa8f828392019b719f
zetalabs <zetalabs@2acfdcce-4352-0410-ad0c-411be7f7a404>
2011-10-02 17:49:25 +0000

    update config for distro lilou target hi351x

---
+++ distro/lilou/testing/customize-rootfs-hi351x-linux-2.6.24.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ROOTFS_DIR=$1
+
+usage() {
+  echo "usage: ./customize-rootfs.sh <rootfs directory>"
+}
+
+dbg() {
+  echo $1
+}
+
+if [ "x$ROOTFS_DIR" = "x" ] ; then
+  usage
+  exit -1
+fi
+
+if [ ! -d $ROOTFS_DIR ] ; then
+  echo "$ROOTFS_DIR does not exist!"
+  exit -1
+fi
+
+# libintl symbol link
+rm -rf $ROOTFS_DIR/lib/libintl.so.8
+ln -sf ../usr/lib/libintl.so.8 $ROOTFS_DIR/lib/libintl.so.8
+
