526fe8dfb3621d51e4748baae133381bfbd826a8
zetalabs <zetalabs@2acfdcce-4352-0410-ad0c-411be7f7a404>
2011-10-13 10:26:05 +0000

    update customize-rootfs script for lilou hi35xx

--- trunk/distro/lilou/testing/customize-rootfs-hi35xx-linux-2.6.24.sh
+++ trunk/distro/lilou/testing/customize-rootfs-hi35xx-linux-2.6.24.sh
@@ -21,6 +21,8 @@ if [ ! -d $ROOTFS_DIR ] ; then
 fi
 
 # libintl symbol link
+rm -rf $ROOTFS_DIR/usr/lib/libintl.so.8
+ln -sf libintl.so.8.1.1 $ROOTFS_DIR/usr/lib/libintl.so.8
 rm -rf $ROOTFS_DIR/lib/libintl.so.8
 ln -sf ../usr/lib/libintl.so.8 $ROOTFS_DIR/lib/libintl.so.8
 
@@ -28,4 +30,18 @@ ln -sf ../usr/lib/libintl.so.8 $ROOTFS_DIR/lib/libintl.so.8
 rm -rf $ROOTFS_DIR/etc
 mkdir -p $ROOTFS_DIR/etc
 
-
+# remove unnecessary kernel modules
+mv -f $ROOTFS_DIR/lib/modules $ROOTFS_DIR/usr/lib/
+mkdir -p $ROOTFS_DIR/lib/modules/2.6.24
+# hi35xx drivers
+mv -f $ROOTFS_DIR/usr/lib/modules/2.6.24/hi35xx_*.ko $ROOTFS_DIR/lib/modules/2.6.24/
+# ethernet driver depends
+mv -f $ROOTFS_DIR/usr/lib/modules/2.6.24/libphy.ko $ROOTFS_DIR/lib/modules/2.6.24/
+# SATA driver
+mv -f $ROOTFS_DIR/usr/lib/modules/2.6.24/ahci.ko $ROOTFS_DIR/lib/modules/2.6.24/
+# EXT4 filesystem drivers
+mv -f $ROOTFS_DIR/usr/lib/modules/2.6.24/ext4dev.ko $ROOTFS_DIR/lib/modules/2.6.24/
+mv -f $ROOTFS_DIR/usr/lib/modules/2.6.24/jbd2.ko $ROOTFS_DIR/lib/modules/2.6.24/
+mv -f $ROOTFS_DIR/usr/lib/modules/2.6.24/mbcache.ko $ROOTFS_DIR/lib/modules/2.6.24/
+# remove the usr/lib/modules directory
+rm -rf $ROOTFS_DIR/usr/lib/modules
