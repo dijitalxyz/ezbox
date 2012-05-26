58211b5f3b4cc886cd8268db435aa1405f30a280
zetalabs <zetalabs@2acfdcce-4352-0410-ad0c-411be7f7a404>
2012-05-26 14:43:19 +0000

    sync with openwrt trunk 20120525

--- trunk/pool/bootstrap/target/linux/ar71xx/generic/profiles/d-link.mk
+++ trunk/pool/bootstrap/target/linux/ar71xx/generic/profiles/d-link.mk
@@ -7,7 +7,7 @@
 
 define Profile/DIR600A1
 	NAME:=D-Link DIR-600 rev. A1
-	PACKAGES:=kmod-ath9k wpad-mini
+	PACKAGES:=
 endef
 
 define Profile/DIR600A1/Description
@@ -16,10 +16,20 @@ endef
 
 $(eval $(call Profile,DIR600A1))
 
+define Profile/DIR601A1
+	NAME:=D-Link DIR-601 rev. A1
+	PACKAGES:=
+endef
+
+define Profile/DIR601A1/Description
+	Package set optimized for the D-Link DIR-601 rev. A1.
+endef
+
+$(eval $(call Profile,DIR601A1))
 
 define Profile/DIR615C1
 	NAME:=D-Link DIR-615 rev. C1
-	PACKAGES:=kmod-ath9k wpad-mini
+	PACKAGES:=
 endef
 
 define Profile/DIR615C1/Description
@@ -29,11 +39,21 @@ endef
 $(eval $(call Profile,DIR615C1))
 
 
+define Profile/DIR615E4
+	NAME:=D-Link DIR-615 rev. E4
+	PACKAGES:=
+endef
+
+define Profile/DIR615E4/Description
+	Package set optimized for the D-Link DIR-615 rev. E4.
+endef
+
+$(eval $(call Profile,DIR615E4))
+
+
 define Profile/DIR825B1
 	NAME:=D-Link DIR-825 rev. B1
-	PACKAGES:=\
-		kmod-ath9k wpad-mini kmod-usb-core kmod-usb-ohci kmod-usb2 \
-		kmod-ledtrig-usbdev
+	PACKAGES:=kmod-usb-core kmod-usb-ohci kmod-usb2 kmod-ledtrig-usbdev
 endef
 
 define Profile/DIR825B1/Description
