b75cac53f5c708013a8a596ec12d5ea332c5435d
zetalabs <zetalabs@2acfdcce-4352-0410-ad0c-411be7f7a404>
2013-12-29 16:00:28 +0000

    fix distro zhuanxu x86 build issue

--- trunk/distro/zhuanxu/testing/customize-x86.sh
+++ trunk/distro/zhuanxu/testing/customize-x86.sh
@@ -24,6 +24,10 @@ fi
 do_customize() {
 #  [ ! -d $POOL_DIR/customize/target/linux/generic ] || cp -af $POOL_DIR/customize/target/linux/generic/* $WORK_DIR/target/linux/generic/
 #  [ ! -d $POOL_DIR/customize/target/linux/x86 ] || cp -af $POOL_DIR/customize/target/linux/x86/* $WORK_DIR/target/linux/x86/
+  if [ -d $WORK_DIR/target/linux/generic/base-files ] ; then \
+    rm -rf $WORK_DIR/target/linux/generic/base-files/init ; \
+    ln -sf sbin/ezcd $WORK_DIR/target/linux/generic/base-files/init ; \
+  fi
   echo "do_customize OK!"
 }
 
