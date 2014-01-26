d9eff1c862ff3fe5eb74872ba3407d6c5e339803
zetalabs <zetalabs@2acfdcce-4352-0410-ad0c-411be7f7a404>
2014-01-26 09:46:17 +0000

    sync with openwrt trunk 20140125

--- pool/bootstrap/target/linux/ar71xx/base-files/lib/ar71xx.sh
+++ pool/bootstrap/target/linux/ar71xx/base-files/lib/ar71xx.sh
@@ -53,6 +53,15 @@ wndr3700_board_detect() {
 	AR71XX_MODEL="$machine"
 }
 
+cybertan_get_hw_magic() {
+	local part
+
+	part=$(find_mtd_part firmware)
+	[ -z "$part" ] && return 1
+
+	dd bs=8 count=1 skip=0 if=$part 2>/dev/null | hexdump -v -n 8 -e '1/1 "%02x"'
+}
+
 tplink_get_hwid() {
 	local part
 
@@ -85,6 +94,9 @@ tplink_board_detect() {
 	"070300"*)
 		model="TP-Link TL-WR703N"
 		;;
+	"071000"*)
+		model="TP-Link TL-WR710N"
+		;;
 	"072001"*)
 		model="TP-Link TL-WR720N"
 		;;
@@ -100,6 +112,9 @@ tplink_board_detect() {
 	"074300"*)
 		model="TP-Link TL-WR743N/ND"
 		;;
+	"075000"*)
+		model="TP-Link TL-WA750RE"
+		;;
 	"080100"*)
 		model="TP-Link TL-WA801N/ND"
 		;;
@@ -111,6 +126,9 @@ tplink_board_detect() {
 		;;
 	"084200"*)
 		model="TP-Link TL-WR842N/ND"
+		;;
+	"085000"*)
+		model="TP-Link TL-WA850RE"
 		;;
 	"090100"*)
 		model="TP-Link TL-WA901N/ND"
@@ -138,6 +156,9 @@ tplink_board_detect() {
 	"001101"*)
 		model="TP-Link TL-MR11U"
 		;;
+	"001301"*)
+		model="TP-Link TL-MR13U"
+		;;
 	"302000"*)
 		model="TP-Link TL-MR3020"
 		;;
@@ -161,6 +182,9 @@ tplink_board_detect() {
 		;;
 	"431000"*)
 		model="TP-Link TL-WDR4310"
+		;;
+	"453000"*)
+		model="MERCURY MW4530R"
 		;;
 	*)
 		hwver=""
@@ -237,6 +261,9 @@ ar71xx_board_detect() {
 	*"DB120 reference board")
 		name="db120"
 		;;
+	*"DIR-505 rev. A1")
+		name="dir-505-a1"
+		;;
 	*"DIR-600 rev. A1")
 		name="dir-600-a1"
 		;;
@@ -271,7 +298,16 @@ ar71xx_board_detect() {
 		name="jwap003"
 		;;
 	*"Hornet-UB")
-		name="hornet-ub"
+		local size
+		size=$(awk '/firmware/ { print $2 }' /proc/mtd)
+
+		if [ "x$size" = "x00790000" ]; then
+			name="hornet-ub"
+		fi
+
+		if [ "x$size" = "x00f90000" ]; then
+			name="hornet-ub-x2"
+		fi
 		;;
 	*LS-SR71)
 		name="ls-sr71"
@@ -281,6 +317,15 @@ ar71xx_board_detect() {
 		;;
 	*MR600)
 		name="mr600"
+		;;
+	*"My Net N600")
+		name="mynet-n600"
+		;;
+	*"My Net N750")
+		name="mynet-n750"
+		;;
+	*"WD My Net Wi-Fi Range Extender")
+		name="mynet-rext"
 		;;
 	*MZK-W04NU)
 		name="mzk-w04nu"
@@ -381,11 +426,17 @@ ar71xx_board_detect() {
 	*TEW-712BR)
 		name="tew-712br"
 		;;
+	*TEW-732BR)
+		name="tew-732br"
+		;;
 	*"TL-WR1041N v2")
 		name="tl-wr1041n-v2"
 		;;
 	*TL-WR1043ND)
 		name="tl-wr1043nd"
+		;;
+	*"TL-WR1043ND v2")
+		name="tl-wr1043nd-v2"
 		;;
 	*TL-WR2543N*)
 		name="tl-wr2543n"
@@ -399,6 +450,9 @@ ar71xx_board_detect() {
 	*TL-MR3040)
 		name="tl-mr3040"
 		;;
+	*"TL-MR3040 v2")
+		name="tl-mr3040-v2"
+		;;
 	*TL-MR3220)
 		name="tl-mr3220"
 		;;
@@ -411,14 +465,26 @@ ar71xx_board_detect() {
 	*"TL-MR3420 v2")
 		name="tl-mr3420-v2"
 		;;
+	*TL-WA750RE)
+		name="tl-wa750re"
+		;;
 	*TL-WA7510N)
 		name="tl-wa7510n"
+		;;
+	*TL-WA850RE)
+		name="tl-wa850re"
+		;;
+	*"TL-WA801ND v2")
+		name="tl-wa801nd-v2"
 		;;
 	*TL-WA901ND)
 		name="tl-wa901nd"
 		;;
 	*"TL-WA901ND v2")
 		name="tl-wa901nd-v2"
+		;;
+	*"TL-WA901ND v3")
+		name="tl-wa901nd-v3"
 		;;
 	*"TL-WDR3500")
 		name="tl-wdr3500"
@@ -441,17 +507,29 @@ ar71xx_board_detect() {
 	*"TL-WR841N/ND v8")
 		name="tl-wr841n-v8"
 		;;
+	*"TL-WR842N/ND v2")
+		name="tl-wr842n-v2"
+		;;
 	*TL-WR941ND)
 		name="tl-wr941nd"
 		;;
 	*"TL-WR703N v1")
 		name="tl-wr703n"
 		;;
+	*"TL-WR710N v1")
+		name="tl-wr710n"
+		;;
 	*"TL-WR720N v3")
 		name="tl-wr720n-v3"
 		;;
+	*"TL-MR10U")
+		name="tl-mr10u"
+		;;
 	*"TL-MR11U")
 		name="tl-mr11u"
+		;;
+	*"TL-MR13U")
+		name="tl-mr13u"
 		;;
 	*UniFi)
 		name="unifi"
@@ -492,6 +570,12 @@ ar71xx_board_detect() {
 	*WNR2000)
 		name="wnr2000"
 		;;
+	*WNR2200)
+		name="wnr2200"
+		;;
+	*"WNR612 V2")
+		name="wnr612-v2"
+		;;
 	*WRT160NL)
 		name="wrt160nl"
 		;;
@@ -524,6 +608,15 @@ ar71xx_board_detect() {
 		;;
 	*EmbWir-Dorin-Router)
 		name="ew-dorin-router"
+		;;
+	"8devices Carambola2"*)
+		name="carambola2"
+		;;
+	*"Sitecom WLR-8100")
+		name="wlr8100"
+		;;
+	*"BHU BXU2000n-2 rev. A1")
+		name="bxu2000n-2-a1"
 		;;
 	esac
 
--- pool/bootstrap/target/linux/ar71xx/base-files/lib/upgrade/platform.sh
+++ pool/bootstrap/target/linux/ar71xx/base-files/lib/upgrade/platform.sh
@@ -49,7 +49,7 @@ platform_do_upgrade_combined() {
 
 	if [ -n "$partitions" ] && [ -n "$kernelpart" ] && \
 	   [ ${kern_blocks:-0} -gt 0 ] && \
-	   [ ${root_blocks:-0} -gt ${kern_blocks:-0} ] && \
+	   [ ${root_blocks:-0} -gt 0 ] && \
 	   [ ${erase_size:-0} -gt 0 ];
 	then
 		local append=""
@@ -67,6 +67,43 @@ tplink_get_image_hwid() {
 
 tplink_get_image_boot_size() {
 	get_image "$@" | dd bs=4 count=1 skip=37 2>/dev/null | hexdump -v -n 4 -e '1/1 "%02x"'
+}
+
+seama_get_type_magic() {
+	get_image "$@" | dd bs=1 count=4 skip=53 2>/dev/null | hexdump -v -n 4 -e '1/1 "%02x"'
+}
+
+cybertan_get_image_magic() {
+	get_image "$@" | dd bs=8 count=1 skip=0  2>/dev/null | hexdump -v -n 8 -e '1/1 "%02x"'
+}
+
+cybertan_check_image() {
+	local magic="$(cybertan_get_image_magic "$1")"
+	local fw_magic="$(cybertan_get_hw_magic)"
+
+	[ "$fw_magic" != "$magic" ] && {
+		echo "Invalid image, ID mismatch, got:$magic, but need:$fw_magic"
+		return 1
+	}
+
+	return 0
+}
+
+platform_do_upgrade_compex() {
+	local fw_file=$1
+	local fw_part=$PART_NAME
+	local fw_mtd=$(find_mtd_part $fw_part)
+	local fw_length=0x$(dd if="$fw_file" bs=2 skip=1 count=4 2>/dev/null)
+	local fw_blocks=$(($fw_length / 65536))
+
+	if [ -n "$fw_mtd" ] &&  [ ${fw_blocks:-0} -gt 0 ]; then
+		local append=""
+		[ -f "$CONF_TAR" -a "$SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
+
+		sync
+		dd if="$fw_file" bs=64k skip=1 count=$fw_blocks 2>/dev/null | \
+			mtd $append write - "$fw_part"
+	fi
 }
 
 platform_check_image() {
@@ -94,6 +131,7 @@ platform_check_image() {
 	ap96 | \
 	db120 | \
 	hornet-ub | \
+	bxu2000n-2-a1 | \
 	zcn-1523h-2 | \
 	zcn-1523h-5)
 		[ "$magic_long" != "68737173" -a "$magic_long" != "19852003" ] && {
@@ -105,6 +143,7 @@ platform_check_image() {
 	ap81 | \
 	ap83 | \
 	ap132 | \
+	dir-505-a1 | \
 	dir-600-a1 | \
 	dir-615-c1 | \
 	dir-615-e4 | \
@@ -112,10 +151,12 @@ platform_check_image() {
 	dir-835-a1 | \
 	ew-dorin | \
 	ew-dorin-router | \
+	hornet-ub-x2 | \
 	mzk-w04nu | \
 	mzk-w300nh | \
 	tew-632brp | \
 	tew-712br | \
+	tew-732br | \
 	wrt400n | \
 	airrouter | \
 	bullet-m | \
@@ -133,7 +174,8 @@ platform_check_image() {
 	wlae-ag300n | \
 	nbg460n_550n_550nh | \
 	unifi | \
-	unifi-outdoor )
+	unifi-outdoor | \
+	carambola2 )
 		[ "$magic" != "2705" ] && {
 			echo "Invalid image type."
 			return 1
@@ -146,6 +188,27 @@ platform_check_image() {
 		dir825b_check_image "$1" && return 0
 		;;
 
+	mynet-rext|\
+	wrt160nl)
+		cybertan_check_image "$1" && return 0
+		return 1
+		;;
+
+	mynet-n600 | \
+	mynet-n750)
+		[ "$magic_long" != "5ea3a417" ] && {
+			echo "Invalid image, bad magic: $magic_long"
+			return 1
+		}
+
+		local typemagic=$(seama_get_type_magic "$1")
+		[ "$typemagic" != "6669726d" ] && {
+			echo "Invalid image, bad type: $typemagic"
+			return 1
+		}
+
+		return 0;
+		;;
 	mr600 | \
 	mr600v2 | \
 	om2p | \
@@ -156,28 +219,38 @@ platform_check_image() {
 		;;
 
 	archer-c7 | \
+	tl-mr10u | \
 	tl-mr11u | \
+	tl-mr13u | \
 	tl-mr3020 | \
 	tl-mr3040 | \
+	tl-mr3040-v2 | \
 	tl-mr3220 | \
 	tl-mr3220-v2 | \
 	tl-mr3420 | \
 	tl-mr3420-v2 | \
 	tl-wa7510n | \
+	tl-wa750re | \
+	tl-wa850re | \
+	tl-wa801nd-v2 | \
 	tl-wa901nd | \
 	tl-wa901nd-v2 | \
+	tl-wa901nd-v3 | \
 	tl-wdr3500 | \
 	tl-wdr4300 | \
 	tl-wr703n | \
+	tl-wr710n | \
 	tl-wr720n-v3 | \
 	tl-wr741nd | \
 	tl-wr741nd-v4 | \
 	tl-wr841n-v1 | \
 	tl-wr841n-v7 | \
 	tl-wr841n-v8 | \
+	tl-wr842n-v2 | \
 	tl-wr941nd | \
 	tl-wr1041n-v2 | \
 	tl-wr1043nd | \
+	tl-wr1043nd-v2 | \
 	tl-wr2543n)
 		[ "$magic" != "0100" ] && {
 			echo "Invalid image type."
@@ -212,19 +285,13 @@ platform_check_image() {
 		}
 		return 0
 		;;
-	wndr3700)
+	wndr3700 | \
+	wnr612-v2)
 		local hw_magic
 
 		hw_magic="$(ar71xx_get_mtd_part_magic firmware)"
 		[ "$magic_long" != "$hw_magic" ] && {
 			echo "Invalid image, hardware ID mismatch, hw:$hw_magic image:$magic_long."
-			return 1
-		}
-		return 0
-		;;
-	wrt160nl)
-		[ "$magic" != "4e4c" ] && {
-			echo "Invalid image type."
 			return 1
 		}
 		return 0
@@ -238,7 +305,9 @@ platform_check_image() {
 	eap7660d | \
 	ja76pf | \
 	ja76pf2 | \
-	jwap003)
+	jwap003 | \
+	wp543 | \
+	wpe72)
 		[ "$magic" != "4349" ] && {
 			echo "Invalid image. Use *-sysupgrade.bin files on this board"
 			return 1
@@ -277,6 +346,10 @@ platform_do_upgrade() {
 	jwap003)
 		platform_do_upgrade_combined "$ARGV"
 		;;
+	wp543|\
+	wpe72)
+		platform_do_upgrade_compex "$ARGV"
+		;;
 	all0258n )
 		platform_do_upgrade_allnet "0x9f050000" "$ARGV"
 		;;
@@ -296,6 +369,10 @@ platform_do_upgrade() {
 	om2p-hs | \
 	om2p-lc)
 		platform_do_upgrade_openmesh "$ARGV"
+		;;
+	uap-pro)
+		MTD_CONFIG_ARGS="-s 0x180000"
+		default_do_upgrade "$ARGV"
 		;;
 	*)
 		default_do_upgrade "$ARGV"
